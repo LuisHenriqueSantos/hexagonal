150267f1ace0c882ca1f6145dbed6c785c2b27d3
LuisHenriqueSantos <luishenriquesantos323@gmail.com>
2026-06-03 07:46:06 -0300

    Finalizando a criação da porta de entrada adapter de deletar.

--- src/main/java/br/com/luis/hexagonal/application/core/usecase/DeleteCustomerByIdUseCase.java
+++ src/main/java/br/com/luis/hexagonal/application/core/usecase/DeleteCustomerByIdUseCase.java
@@ -1,23 +1,24 @@
 package br.com.luis.hexagonal.application.core.usecase;
 
 import br.com.luis.hexagonal.application.ports.in.FindCustomerInportPort;
+import br.com.luis.hexagonal.application.ports.out.DeleteCustomerByIdOutputPort;
 
 public class DeleteCustomerByIdUseCase {
     
     private final FindCustomerInportPort findCustomerInportPort;
     
-    private final DeleteCustomerByIdUseCase deleteCustomerByIdUseCase;
+    private final DeleteCustomerByIdOutputPort deleteCustomerByIdOutputPort;
 
-    public DeleteCustomerByIdUseCase(FindCustomerInportPort findCustomerInportPort,
-                                     DeleteCustomerByIdUseCase deleteCustomerByIdUseCase
+    public DeleteCustomerByIdUseCase(FindCustomerInportPort findCustomerInportPort, 
+                                     DeleteCustomerByIdOutputPort deleteCustomerByIdOutputPort
     ) {
         this.findCustomerInportPort = findCustomerInportPort;
-        this.deleteCustomerByIdUseCase = deleteCustomerByIdUseCase;
+        this.deleteCustomerByIdOutputPort = deleteCustomerByIdOutputPort;
     }
 
 
     public void delete(String id){
         findCustomerInportPort.find(id);
-        deleteCustomerByIdUseCase.delete(id);
+        deleteCustomerByIdOutputPort.delete(id);
     }
 }
