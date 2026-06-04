9fbf40f0e4d8aca25df0c4efd21a137ddaaabbf4
LuisHenriqueSantos <luishenriquesantos323@gmail.com>
2026-06-04 18:29:19 -0300

    Update

--- src/main/java/br/com/luis/hexagonal/adapters/out/repository/entity/AddressEntity.java
+++ src/main/java/br/com/luis/hexagonal/adapters/out/repository/entity/AddressEntity.java
@@ -1,10 +1,8 @@
 package br.com.luis.hexagonal.adapters.out.repository.entity;
 
 import lombok.Data;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@Document(collation = "address")
 public class AddressEntity {
 
     private String street;
