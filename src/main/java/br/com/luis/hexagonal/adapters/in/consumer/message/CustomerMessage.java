fc932f8cb9358cf7905a7b514c311d04e5e7d88b
LuisHenriqueSantos <luishenriquesantos323@gmail.com>
2026-06-04 00:30:31 -0300

    Update CustomerMessage.java

--- src/main/java/br/com/luis/hexagonal/adapters/in/consumer/message/CustomerMessage.java
+++ src/main/java/br/com/luis/hexagonal/adapters/in/consumer/message/CustomerMessage.java
@@ -4,9 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class CustomerMessage {
     
     private String id;
@@ -21,53 +18,53 @@ public class CustomerMessage {
     
     public CustomerMessage(){}
     
-//    public CustomerMessage(
-//            String id, String name, String zipCode, String cpf, Boolean isValidCpf
-//    ){
-//        this.id = id;
-//        this.name = name; 
-//        this.zipCode = zipCode;
-//        this.cpf = cpf; 
-//        this.isValidCpf = isValidCpf;
-//    }
-//
-//    public String getId() {
-//        return id;
-//    }
-//
-//    public void setId(String id) {
-//        this.id = id;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public String getZipCode() {
-//        return zipCode;
-//    }
-//
-//    public void setZipCode(String zipCode) {
-//        this.zipCode = zipCode;
-//    }
-//
-//    public String getCpf() {
-//        return cpf;
-//    }
-//
-//    public void setCpf(String cpf) {
-//        this.cpf = cpf;
-//    }
-//
-//    public Boolean getValidCpf() {
-//        return isValidCpf;
-//    }
-//
-//    public void setValidCpf(Boolean validCpf) {
-//        isValidCpf = validCpf;
-//    }
+    public CustomerMessage(
+            String id, String name, String zipCode, String cpf, Boolean isValidCpf
+    ){
+        this.id = id;
+        this.name = name; 
+        this.zipCode = zipCode;
+        this.cpf = cpf; 
+        this.isValidCpf = isValidCpf;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Boolean getValidCpf() {
+        return isValidCpf;
+    }
+
+    public void setValidCpf(Boolean validCpf) {
+        isValidCpf = validCpf;
+    }
 }
